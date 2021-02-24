fecf2dc1944c76aa3b700f4c9a681f3bf6edec02
Anderson <andersoninfonet@gmail.com>
2021-02-24 00:18:25 -0300

    🗃  add database support

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid'
+
+@Entity('users')
+class User {
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+    }
+
+    @PrimaryColumn()
+    readonly id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date
+}
+
+export default User
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
-import express, { Request, Response} from 'express'
+import 'reflect-metadata'
+import './database'
+import express from 'express'
+import router from './routes'
 
 const app = express()
 
-app.get('/', (request: Request, response: Response) => {
-    return response.json({
-        message: 'Hello World!'
-    })
-})
-
-app.post('/', (request: Request, response: Response) => {
-    return response.json({
-        message: 'Os dados foram salvos com sucesso !'
-    })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log('Server is running'))
